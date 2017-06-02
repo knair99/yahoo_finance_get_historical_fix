e0b8598290c6b7baad22911cf9626ead94820baa
Karunakaran Prasad <kprasad@Karunakarans-MacBook-Air.local>
2017-06-02 13:04:42 -0700

    fixed comma in number parsing

--- get_history.py
+++ get_history.py
@@ -9,11 +9,11 @@ def get_historical_data(name, number_of_days):
 	for each_row in rows:
 		divs = each_row.findAll('td')
 		if divs[1].span.text  != 'Dividend': #Ignore this row in the table
-			#I'm only interested in 'Open'; For other values, play with divs[1 through 5]
-			data.append({'Date': divs[0].span.text, 'Open': float(divs[1].span.text)})
+			#I'm only interested in 'Open' price; For other values, play with divs[1 - 5]
+			data.append({'Date': divs[0].span.text, 'Open': float(divs[1].span.text.replace(',',''))})
 
 	return data[:number_of_days]
 
 #Test
-for i in get_historical_data('tsla', 5):
-	print i
+for i in get_historical_data('nflx', 5):
+    print i
